4c1401364f301fd9f3a08cc86ddcb12ec8fa8b0b
Benson King'ori <b.mugure@alustudent.com>
2025-06-30 20:48:23 +0400

    Updated reflect.tsx

--- pages/reflect.tsx
+++ pages/reflect.tsx
@@ -19,15 +19,7 @@ export default function ReflectPage() {
       setIsRedirecting(true); // Prevent additional redirects
       router.push('/');
     }
-  }, [user, loading, isRedirecting, router]);
-
-  if (loading || isRedirecting) {
-    return (
-      <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-50 to-indigo-100 dark:from-gray-900 dark:to-gray-800">
-        <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-blue-600"></div>
-      </div>
-    );
-  }
+  }, [user, isRedirecting, router]);
 
   if (!user) {
     return null; // Will redirect if no user
