447baa572b24c2a3ff18f5035725ba9b4107884a
Benson King'ori <b.mugure@alustudent.com>
2025-06-30 15:00:00 +0400

    Fix date-fns-tz import error

--- lib/reflections.ts
+++ lib/reflections.ts
@@ -1,7 +1,7 @@
 import { supabase } from './supabase';
 import { ReflectionCrypto } from './crypto';
 import { startOfWeek, addDays, isAfter, format } from 'date-fns';
-import { zonedTimeToUtc, utcToZonedTime } from 'date-fns-tz/esm';
+import { zonedTimeToUtc, utcToZonedTime } from 'date-fns-tz';
 
 export interface ReflectionAnswers {
   question1: string;
