df18ab1c2a33f114f13297b3b669600c41ff9ac0
Benson King'ori <b.mugure@alustudent.com>
2025-06-30 19:21:21 +0400

    Fix styling by adding global CSS and environment variables

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from '@/components/ui/theme-provider';
 import { AuthProvider } from '@/contexts/AuthContext';
